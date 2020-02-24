3d7b577fec1e70df409e4945ac132f7b16418947
kyle67 <daniels.kyle@gmail.com>
2020-02-24 12:46:57 -0500

    Modified control flow for leap year code challenge

--- leapYearChallenge.js
+++ leapYearChallenge.js
@@ -2,16 +2,48 @@
 
 // leap year = year%4===0 && year%100===0 && year%400===0
 // 
-    function isLeap(Year){
+//     function isLeap(Year){
 
-    if ((Year%100!==0) && (Year%4==0) || (Year%400===0)){
-       alert("This is a leap year");
-    }
+//     if ((Year%100!==0) && (Year%4==0) || (Year%400===0)){
+//        alert("This is a leap year");
+//     }
     
-    else{
+//     else{
 
-        alert("This is not a leap year");
-    }
+//         alert("This is not a leap year");
+//     }
+// }
+
+// isLeap(2020);
+
+// function isLeap(year){
+//     if(year%4===0){
+//      if(year%100===0){
+//          if(year%400===0){
+//              return "leap year";
+//          } else {
+//              return "not a leap year"
+//          }
+//      } else{
+//          return "leap year"
+//      }
+//     } else{
+//         return "not a leap year";
+//     }
+// }
+
+function isLeap(year){
+   if (year%4===0){
+     if (year%100===0){
+       if(year%400===0){
+           return "leap year"
+       } else{
+           return "not a leap year"
+       }
+     }else {
+         return "leap year"
+     }
+   } else{
+       return "not a leap"
+   }
 }
-
-isLeap(2020);
