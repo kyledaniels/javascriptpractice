7f47f5b756df039477bfc19b358adf4cf4346962
kyle67 <daniels.kyle@gmail.com>
2020-02-25 09:29:52 -0500

    Modified control flow for fizzbuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -7,8 +7,10 @@ var count = 1;
 
 function fizzBuzz(){
 
- 
- if(count%3===0){
+ if(count%3===0 && count%5===0){
+   output.push("fizzbizz")
+ }
+  else if(count%3===0){
    output.push("fizz")
  } 
  else if (count%5===0){
