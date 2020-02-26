579798538b9065cc2d7c208665f9ccb1dea1f482
kyle67 <daniels.kyle@gmail.com>
2020-02-26 07:13:26 -0500

    modified fizzBuzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -2,27 +2,30 @@
 // Buzz for every multiple of 5
 // FizzBuzz for every number that is a multiple of 3 and 5
 
-var output = []
+
+
+
+var output = [];
 var count = 1;
 
 function fizzBuzz(){
 
- if(count%3===0 && count%5===0){
-   output.push("fizzbizz")
- }
-  else if(count%3===0){
-   output.push("fizz")
- } 
- else if (count%5===0){
-   output.push("bizz")
- }
- else{
-   output.push(number)
- }
-
-
+  if(count%3===0 && count%5===0){
+    output.push("fizzbuzz")
+  } 
   
+  else if (count%3===0){
+    output.push("fizz")
+  } 
+  
+  else if (count%5===0){
+    output.push("buzz")
+  }
+   else{
+    outpush.push(count)
+  }
+  count++
   console.log(output);
-}
 
+}
 
