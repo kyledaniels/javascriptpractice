ff9d17b06661829abdf807c630d3a4d627b471ad
kyle67 <daniels.kyle@gmail.com>
2020-02-26 08:03:16 -0500

    modifed guestList

--- guestList.js
+++ guestList.js
@@ -1,9 +1,19 @@
 var guestList = ["Angela","Jack","Pam","James","Lara","Jason"];
 
 
+// var name = prompt("What is your name?")
+// if(guestList.includes(name)){
+//     console.log("Welcome to the party")
+// } else {
+//     console.log ("maybe next time")
+// }
+
+var guestList = ["Angela","Jack","Pam","James","Lara","Jason"];
+
 var name = prompt("What is your name?")
-if(guestList.includes(name)){
+
+if (guestList.includes(name)){
     console.log("Welcome to the party")
 } else {
-    console.log ("maybe next time")
+    console.log("maybe next time")
 }
