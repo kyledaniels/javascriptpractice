3047fcb8d7cf286d0af1ec906bb0c563cf632c1b
kyle67 <daniels.kyle@gmail.com>
2020-02-26 07:33:43 -0500

    modified leapYear

--- leapYearChallenge.js
+++ leapYearChallenge.js
@@ -1,63 +1,21 @@
 //Write a code that will determine if the given year is a leap year. 
 
-// leap year = year%4===0 && year%100===0 && year%400===0
-// 
-//     function isLeap(Year){
+// leap year = year%4===0 && year%100!==0 && year%400===0
 
-//     if ((Year%100!==0) && (Year%4==0) || (Year%400===0)){
-//        alert("This is a leap year");
-//     }
-    
-//     else{
-
-//         alert("This is not a leap year");
-//     }
-// }
-
-// isLeap(2020);
-
-// function isLeap(year){
-//     if(year%4===0){
-//      if(year%100===0){
-//          if(year%400===0){
-//              return "leap year";
-//          } else {
-//              return "not a leap year"
-//          }
-//      } else{
-//          return "leap year"
-//      }
-//     } else{
-//         return "not a leap year";
-//     }
-// }
-
-// function isLeap(year){
-//    if (year%4===0){
-//      if (year%100===0){
-//        if(year%400===0){
-//            return "leap year"
-//        } else{
-//            return "not a leap year"
-//        }
-//      }else {
-//          return "leap year"
-//      }
-//    } else{
-//        return "not a leap"
-//    }
-// }
 
 function isLeap(year){
   if(year%4===0){
-    if(year%100===0)
-     if(year%400===0){
-       return "leap year"
-     } else{
-       return "no leap year"
-     }
-     {
-      return "leap year"
-    }
-  } return "no leap year"
+    if(year%100===0){
+      if(year%400===0){
+        return "leap year"
+      } else {
+        "no leap year"}
+    } 
+    else{
+      return "leap year"}
+  }
+  else{
+    return "no leap year"}
 }
+  
+    
