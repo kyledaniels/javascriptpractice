771a77d1943dca18b559b29e52b7ba9eab486792
kyle67 <daniels.kyle@gmail.com>
2020-03-05 09:21:42 -0500

    modified control flow for Fizz Buzz

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -5,27 +5,50 @@
 
 
 
+// var output = [];
+// var count = 1;
+
+// function fizzBuzz(){
+
+//   if(count%3===0 && count%5===0){
+//     output.push("fizzbuzz")
+//   } 
+  
+//   else if (count%3===0){
+//     output.push("fizz")
+//   } 
+  
+//   else if (count%5===0){
+//     output.push("buzz")
+//   }
+//    else{
+//     outpush.push(count)
+//   }
+//   count++
+//   console.log(output);
+
+// }
+
+
 var output = [];
 var count = 1;
 
-function fizzBuzz(){
+function fizzBuza(){
 
-  if(count%3===0 && count%5===0){
-    output.push("fizzbuzz")
+if(count%3===0 && count%5===0){
+  output.push("FizzBuzz");
+} 
+  else if(count%3===0){
+   output.push("Fizz")
   } 
-  
-  else if (count%3===0){
-    output.push("fizz")
+  else if(count%5===0){
+    output.push("Buzz")
   } 
-  
-  else if (count%5===0){
-    output.push("buzz")
-  }
    else{
-    outpush.push(count)
-  }
-  count++
-  console.log(output);
+     output.push("count")
+   }
+    count++; 
+    console.log(output);
+
 
 }
-
