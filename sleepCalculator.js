f8e02183cf31cb82ab18730b48c4732d4831f4ad
kyle67 <daniels.kyle@gmail.com>
2020-03-22 17:40:24 -0400

    created function to calculate actual hours of sleep

--- sleepCalculator.js
+++ sleepCalculator.js
@@ -1,22 +1,26 @@
 function getSleepHours(day){
     switch(day){
-        case 'Monday':
-        return 5
-        break;
-        case 'Tuesday':
-        return 6
-        break;
-        case 'Wednesday':
-        return 7
-        break;
-        case 'Thursday':
-        return 8
-        break;
-        case 'Friday':
-        return 9
-        break;
-        default:
-        error
-        break;
-      }
-}
+      case 'Monday':
+      return 5
+     
+      break;
+      case 'Tuesday':
+      return 6
+      break;
+      case 'Wednesday':
+      return 7
+      break;
+      case 'Thursday':
+      return 8
+      break;
+      case 'Friday':
+      return 9
+      break;
+      default:
+      error
+      break;
+    }
+   }
+   function getActualSleepHours(){
+      getSleepHours()
+   }
