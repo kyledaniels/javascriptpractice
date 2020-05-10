7c7c260ff4bd9a51f18e5f0c35f22f1370177f5e
kyle67 <daniels.kyle@gmail.com>
2020-05-10 18:00:19 -0400

    created dish object with addDishToCourse method

--- menu.js
+++ menu.js
@@ -3,18 +3,45 @@ const menu = {
        appetizers:[],
        mains:[],
        desserts:[]
-    }
+    },
     get appetizers(){
         return this._courses.appetizers;
     },
     
     get mains(){
          return this._courses.mains;
-    }
+    },
   
     get desserts(){
          return this._courses.desserts;
-    }
-  
+    },
+
+    set appetizers(appetizers){
+        this._courses.appetizers= appetizers;
+    },
+
+    set mains(mains){
+        this._courses.mains= mains;
+    },
+
+    set desserts(desserts){
+        this._coursers.desserts= desserts;
+    },
+
+get courses(){
+  return {
+    appetizers:this.appetizers,
+    mains:this.mains,
+    desserts:this.desserts
+  }
+},
+
+addDishToCourse(courseName,dishName,dishPrice){
+   const dish ={
+     name:dishName,
+     price:dishPrice,
+   }
+    return this._courses[courseName].push(dish);
+}
     
   };
