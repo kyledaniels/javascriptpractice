5d6930d1b14542b5559e26dbaec8c3c10570abea
kyle67 <daniels.kyle@gmail.com>
2020-09-27 14:14:01 -0400

    called the menu object to generate meal and saved to variable meal

--- menu.js
+++ menu.js
@@ -1,64 +1,62 @@
 const menu = {
-    _courses:{
-       appetizers:[],
-       mains:[],
-       desserts:[]
-    },
-    get appetizers(){
-        return this._courses.appetizers;
-    },
-    
-    get mains(){
-         return this._courses.mains;
-    },
-  
-    get desserts(){
-         return this._courses.desserts;
-    },
+  _courses:{
+    appetizers:[],
+    mains:[],
+    desserts:[]
+  },
 
-    set appetizers(appetizers){
-        this._courses.appetizers= appetizers;
-    },
+  get appetizers (){
+    return this._courses.appetizers;
+  },
 
-    set mains(mains){
-        this._courses.mains= mains;
-    },
+  set appetizers(appetizerChoice){
+    this._courses.appetizers=appetizerChoice;
+  },
 
-    set desserts(desserts){
-        this._coursers.desserts= desserts;
-    },
+  get mains (){
+    return this._courses.mains;
+  },
 
-get courses(){
-  return {
-    appetizers:this.appetizers,
-    mains:this.mains,
-    desserts:this.desserts
-  }
-},
+  set mains(mainChoice){
+    this._courses.mains=mainChoice;
+  },
 
-addDishToCourse(courseName,dishName,dishPrice){
-   const dish ={
+  get desserts (){
+    return this._courses.desserts;
+  },
+
+  set desserts(dessertChoice){
+    this._courses.desserts=dessertChoice
+  },
+
+  get courses (){
+    return {appetizers:this.appetizers,
+            mains:this.mains,
+            desserts:this.desserts};
+  },
+
+  addDishToCourse (courseName,dishName,dishPrice) {
+   const dish = {
      name:dishName,
      price:dishPrice,
-   }
-    return this._courses[courseName].push(dish);
-},
-
-getRandomDishFromCourse(courseName){
-    const dishes = this.courses[courseName];
-    const randomIndex = Math.floor(Math.random() * dishes.length);
-    return dishes[randomIndex]
-
+   };
+  return this._courses[courseName].push(dish);
   },
+
+  getRandomDishFromCourse(courseName){
+  const dishes = this._courses[courseName];
+  const randomIndex =  Math.floor(Math.random() * dishes.length );
+  return dishes[randomIndex];
+  },
+
   generateRandomMeal(){
-      const appetizer = this.getRandomDishFromCourse('appetizers');
-      const main = this.getRandomDishFromCourses('mains');
-      const dessert = this.getRandomDishFromCourses('desserts');
-      const totalPrice= appetizer.price - main.price + dessert.price;
-      return `Your meal is ${appetizer.name}, ${main.name}, and ${dessert.name},
-      and the total price is ${totalPrice}`;
-  }
-};
+       const appetizer = this.getRandomDishFromCourse('appetizerChoice');
+       const main = this.getRandomDishFromCourse('mainChoice');
+       const dessert = this.getRandomDishFromCourse('dessertChoice');
+       const totalPrice = appetizer.price + main.price + dessert.price;
+       return `Your meal is ${appetizer.name},${main.name},${dessert.name}, and the total price is ${totalPrice}`;
+  },
+}
 
 menu.addDishToCourse('appetizers','salad',4.00);
 menu.addDishToCourse('appetizers','wings',5.00);
@@ -71,3 +69,6 @@ menu.addDishToCourse('mains','chicken',10.00);
 menu.addDishToCourse('deserts','coffee',4.00);
 menu.addDishToCourse('deserts','ice cream',5.00);
 menu.addDishToCourse('deserts','cake',6.00);
+
+const meal = menu.generateRandomMeal()
+console.log(meal);
